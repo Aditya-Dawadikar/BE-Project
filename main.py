ae7e3b74ae39bc4187df0d9592d99988c5af8b25
aditya-dawadikar <adityadawadikar2000@gmail.com>
2022-01-14 14:53:33 +0530

    prev commit

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask,send_file, request
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 #importing resources
 from app.RouteResources import Visualization as vis_resources
 from app.RouteResources import Filter as filt_resources
 from app.RouteResources import Classifier as classifier_resources
+from app.RouteResources import SignalProcessor as signalprocessor_resources
 
 #Handling routes
 #visualization
@@ -27,11 +30,20 @@ api.add_resource(classifier_resources.PredictAbnormality,'/classifier/abnormalit
 api.add_resource(classifier_resources.PredictDisorder,'/classifier/disorder')
 api.add_resource(classifier_resources.Visualize_Result,'/classifier/visualize')
 
+#signal processing
+api.add_resource(signalprocessor_resources.Melspectrogram,'/signal/melspectrogram')
+
 #handling page not found error
 @app.errorhandler(404)
 def invalid_route(e):
     return {'error':"404",
         "message":"invalide path"
+    }
+
+@app.errorhandler(400)
+def bad_req(e):
+    return {'error':"400",
+        "message":"bad request"
     }
 
 #main
